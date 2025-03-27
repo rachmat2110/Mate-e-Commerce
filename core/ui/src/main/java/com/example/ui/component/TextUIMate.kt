a6024ab404606279990d3350e8ab8057c53158b2
noerrachmat <noer.rachmat@phillip.co.id>
2025-03-27 14:11:08 +0700

    Component Text Row like Linear Layout (horizontal / vertical)

--- core/ui/src/main/java/com/example/ui/component/TextUIMate.kt
+++ core/ui/src/main/java/com/example/ui/component/TextUIMate.kt
@@ -1,11 +1,14 @@
 package com.example.ui.component
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
@@ -112,22 +115,36 @@ fun MateTextRegular(
 fun MateTextViewRow(
     checked: Boolean = false,
     onCheckedChanged: (Boolean) -> Unit = {},
-    onTextClick: () -> Unit = {}
+    onTextClick: () -> Unit = {},
+    textLeft: String = "Remember me",
+    textRight: String = " Forgot password ?"
 ){
     Row(
-        modifier = Modifier.padding(16.dp)
+        modifier = Modifier.padding(16.dp).fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
     ) {
         MateCheckBox(
             checked = checked,
-            onCheckedChanged = onCheckedChanged
+            onCheckedChanged = onCheckedChanged,
+            label = textLeft,
+            modifier = Modifier.padding(vertical = 8.dp)
+                .weight(1f)
         )
 
         val annotationString = buildAnnotatedString {
-            append(" Forgot password ?")
+            append(textRight)
         }
 
         ClickableText(
             text = annotationString,
+            modifier = Modifier.padding(vertical = 8.dp),
+            style = TextStyle(
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Normal,
+                lineHeight = 14.sp,
+                textAlign = TextAlign.Left
+            ),
             onClick = {
                 onTextClick()
             }
